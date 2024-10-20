9b4659bec30c59b08621a0c3bd33f2beb5b22638
Enge Karthikeya <karthikeya.enge@hotmail.com>
2024-10-20 15:01:56 +0530

    Added the docker resources endpoint

--- fastapi_docker/app/main.py
+++ fastapi_docker/app/main.py
@@ -10,6 +10,11 @@ def read_root():
 def read_item(name: str):
     return {"message": f"Hello, {name}!"}
 
+@app.get("/docker/resources")
+def read_resources():
+    return {"message": ["https://courses.devopsdirective.com/docker-beginner-to-pro","https://docs.docker.com/","https://docs.docker.com/get-started/",]}
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
